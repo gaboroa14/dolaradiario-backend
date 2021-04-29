06a194393da6f8927d38504c7820c34cb3ccf713
Gabriel Roa <gaboroab14@gmail.com>
2021-04-28 20:36:32 -0400

    remove this thing again

--- controllers/prices.ts
+++ controllers/prices.ts
@@ -1,6 +1,4 @@
-import { getAllPrices, insertNewPrice } from "../repositories/pricesRepository.ts";
-import { Status } from "https://deno.land/std@0.94.0/http/http_status.ts";
-import Price from "../models/Price.ts";
+import { getAllPrices } from "../repositories/pricesRepository.ts";
 
 export async function getPrices(ctx: any) {
   const response = await getAllPrices();
